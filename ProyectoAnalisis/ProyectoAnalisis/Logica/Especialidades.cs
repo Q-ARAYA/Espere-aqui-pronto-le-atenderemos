e5c6e63868e245a1cc45867f98f1b57a121f3601
Dcalvog25 <davidcg2508@gmail.com>
2025-05-31 13:18:29 -0600

    Primer Commit de Inicio
    Se adjunta inicio de carpetas, clases, y estructura del proyecto (frontend-backend).

---
+++ ProyectoAnalisis/ProyectoAnalisis/Logica/Especialidades.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAnalisis.Logica
+{
+    public class Especialidades
+    {
+        public Especialidades(string nombre, int duracion)
+        {
+            Nombre = nombre;
+            Duracion = duracion;
+        }
+
+        public string Nombre { get; set; }
+        public int Duracion { get; set; }
+
+    }
+}
