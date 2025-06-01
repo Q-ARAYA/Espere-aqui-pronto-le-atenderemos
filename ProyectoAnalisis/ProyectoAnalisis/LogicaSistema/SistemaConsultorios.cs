986c1f1684c2bd0eabb7602be0f53688edd14468
Dcalvog25 <160780800+Dcalvog25@users.noreply.github.com>
2025-05-31 18:09:56 -0600

    Primer y Segundo Commit
    Debido a errores, se tuvo que eliminar los directorios anteriores. 
    Contiene la logica de carpetas (Logica, Vista, LogicaVista, LogicaSistema), clases Pacientes, Consultorios, Especialidades, contiene la logica del sistema "El programa debe ser capaz de recibir nuevos pacientes, crear consultorios, agregar especialidades a los consultorios además de sacar pacientes de la lista, cerrar consultorios y quitar especialidades de los consultorios."

---
+++ ProyectoAnalisis/ProyectoAnalisis/LogicaSistema/SistemaConsultorios.cs
@@ -0,0 +1,47 @@
+﻿using ProyectoAnalisis.Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAnalisis.LogicaSistema
+{
+    public class SistemaConsultorios
+    {
+        public class GestorConsultorios
+        {
+            public List<Consultorios> Consultorios { get; set; } = new List<Consultorios>();
+
+            public void AgregarConsultorio(string nombre)
+            {
+                if (Consultorios.Count < 15)
+                    Consultorios.Add(new Consultorios(nombre, true));
+            }
+
+            public void CerrarConsultorio(string nombre)
+            {
+                var consultorio = Consultorios.Find(c => c.Nombre == nombre);
+                if (consultorio != null)
+                    consultorio.Activo = false;
+            }
+
+            public void AgregarEspecialidad(string nombreConsultorio, Especialidades nuevaEspecialidad)
+            {
+                var consultorio = Consultorios.Find(c => c.Nombre == nombreConsultorio);
+                if (consultorio != null)
+                    consultorio.Especialidades.Add(nuevaEspecialidad);
+            }
+
+            public void QuitarEspecialidad(string nombreConsultorio, string nombreEspecialidad)
+            {
+                var consultorio = Consultorios.Find(c => c.Nombre == nombreConsultorio);
+                if (consultorio != null)
+                    consultorio.Especialidades.RemoveAll(e => e.Nombre == nombreEspecialidad);
+            }
+        }
+
+
+
+    }
+}
