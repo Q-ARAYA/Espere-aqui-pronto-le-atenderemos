081a8e45c63f097324b9ccbffafff5f539ea4c55
Quiriat Araya <125586179+Q-ARAYA@users.noreply.github.com>
2025-06-01 23:17:33 -0600

    Conexión paquete lógica con paquete vistas

--- ProyectoAnalisis/ProyectoAnalisis/LogicaVistas/LogicaVistaMain.cs
+++ ProyectoAnalisis/ProyectoAnalisis/LogicaVistas/LogicaVistaMain.cs
@@ -1,12 +1,55 @@
-﻿using System;
+using ProyectoAnalisis.Logica;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace ProyectoAnalisis.LogicaVistas
 {
-    internal class LogicaVistaMain
+    public static class LogicaVistaMain
     {
+        public static void MostrarConsultorios(List<Consultorios> consultorios, WrapPanel panel)
+        {
+            panel.Children.Clear();
+            foreach (var consultorio in consultorios)
+            {
+                Border border = new Border
+                {
+                    Width = 120,
+                    Height = 60,
+                    Margin = new System.Windows.Thickness(10),
+                    Background = consultorio.Activo ? Brushes.LightGreen : Brushes.LightGray,
+                    BorderBrush = Brushes.Black,
+                    BorderThickness = new System.Windows.Thickness(1),
+                    Child = new TextBlock
+                    {
+                        Text = consultorio.Nombre,
+                        HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                        VerticalAlignment = System.Windows.VerticalAlignment.Center,
+                        TextAlignment = System.Windows.TextAlignment.Center
+                    }
+                };
+                panel.Children.Add(border);
+            }
+        }
+
+        public static void MostrarPacientes(List<Pacientes> pacientes, WrapPanel panel)
+        {
+            panel.Children.Clear();
+            foreach (var paciente in pacientes)
+            {
+                Ellipse circulo = new Ellipse
+                {
+                    Width = 40,
+                    Height = 40,
+                    Fill = Brushes.LightBlue,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1,
+                    Margin = new System.Windows.Thickness(5),
+                    ToolTip = $"{paciente.Nombre} - Prioridad: {paciente.Prioridad}"
+                };
+                panel.Children.Add(circulo);
+            }
+        }
     }
 }
