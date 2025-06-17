32a99438dc66a36fee290d69666bba0d0a9af7e2
Dcalvog25 <davidcg2508@gmail.com>
2025-06-16 23:52:11 -0600

    Detalles Finales 2.0
    Mas detalles finales como la carga de datos que ya estaba implementado solo faltaba mostrarlo en la interfaz.

--- ProyectoAnalisis/ProyectoAnalisis/Logica/CargarDatos.cs
+++ ProyectoAnalisis/ProyectoAnalisis/Logica/CargarDatos.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ProyectoAnalisis.LogicaVistas;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Xml;
 
 namespace ProyectoAnalisis.Logica
@@ -21,7 +24,21 @@ namespace ProyectoAnalisis.Logica
                 string nombre = nodo.Attributes["nombre"].Value;
                 int duracion = int.Parse(nodo.Attributes["duracion"].Value);
 
-                especialidades.Add(new Especialidades(nombre, duracion));
+                string error = LogicaVistaMain.CrearEspecialidad(nombre, duracion);
+                if (error != null)
+                {
+                    throw new Exception($"Error al cargar especialidad: {error}");
+                }
+
+                var ultimo = LogicaVistaMain.CargarEspecialidades().ElementAtOrDefault(LogicaVistaMain.CargarEspecialidades().Count - 1);
+                if (ultimo != null)
+                {
+                    especialidades.Add(ultimo);
+                }
+                else
+                {
+                    throw new Exception("No se pudo cargar la especialidad desde la lógica.");
+                }
             }
 
             return especialidades;
@@ -36,6 +53,12 @@ namespace ProyectoAnalisis.Logica
             doc.Load(rutaArchivo);
 
             XmlNodeList nodosPacientes = doc.SelectNodes("//Pacientes/Paciente");
+
+            // Obtener la ruta de imágenes para selección aleatoria
+            string rutaBase = AppDomain.CurrentDomain.BaseDirectory;
+            string rutaImagenes = System.IO.Path.GetFullPath(System.IO.Path.Combine(rutaBase, @"..\..\Imagenes"));
+            var imagenes = Directory.GetFiles(rutaImagenes, "*.png").ToList();
+            Random random = new Random();
 
             foreach (XmlNode nodoPaciente in nodosPacientes)
             {
@@ -57,10 +80,31 @@ namespace ProyectoAnalisis.Logica
                     }
                 }
 
-                pacientes.Add(new Pacientes(id, nombre, especialidadesPaciente));
+                var ultimoPacienteID = LogicaVistaMain.ObtenerPacientes().Count;
+                var resultado = LogicaVistaMain.CrearPaciente(ultimoPacienteID, nombre, especialidadesPaciente);
+
+                if (resultado == null) // Si se creó correctamente
+                {
+                    // Obtener el paciente recién creado
+                    var pacienteCreado = LogicaVistaMain.ObtenerPacientes().LastOrDefault();
+                    if (pacienteCreado != null)
+                    {
+                        pacientes.Add(pacienteCreado);
+
+                        // Seleccionar imagen aleatoria
+                        string imagenSeleccionada = imagenes.Count > 0
+                            ? imagenes[random.Next(imagenes.Count)]
+                            : null;
+
+                        // Agregar paciente a la lista de espera
+                        LogicaVistaMain.CrearPacienteEnEspera(pacienteCreado, imagenSeleccionada);
+                    }
+                }
             }
 
             return pacientes;
         }
     }
 }
+
+
